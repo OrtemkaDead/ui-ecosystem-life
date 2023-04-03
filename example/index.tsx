c383ce50f3429dbc18d355ef8d5f1e86025fc257
8 Hokage <56502279+OrtemkaDead@users.noreply.github.com>
2023-04-03 23:06:50 +0500

    pezdec tretiy test

--- example/index.tsx
+++ example/index.tsx
@@ -1,9 +1,16 @@
-import 'react-app-polyfill/ie11';
-import * as React from 'react';
-import * as ReactDOM from 'react-dom';
+import 'react-app-polyfill/ie11'
+import * as React from 'react'
+import * as ReactDOM from 'react-dom'
+
+import { Button } from '../src/Button'
 
 const App = () => {
-  return <div>Example test</div>;
-};
+  return (
+    <>
+      <div>Example test</div>
+      <Button />
+    </>
+  )
+}
 
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(<App />, document.getElementById('root'))
